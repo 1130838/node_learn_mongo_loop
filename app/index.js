c80f3fef7cc769c2ea0697a9a0f91e4111166d78
1130838 <1130838@isep.ipp.pt>
2016-08-12 00:59:40 +0100

    added more loops. now the chainable structure is: (2 will execute only after 1 finish and so forth ):  1. do something firt  2. make first request  3. loop and insert in DB  4. loop making requests
      adding more 'then' can customize at your situation ( i guess )

--- app/index.js
+++ app/index.js
@@ -16,65 +16,139 @@ function loopForInsertInMongoDB() {
 
 function loopForAsynchronousInsertInMongoDB() {
 
-    // promise while pattern function:
-    var promiseWhile = function (condition, action) {
-        var resolver = Promise.defer();
+// U S A G E : #################################################################
 
-        var loop = function () {
-            if (!condition()) return resolver.resolve();
-            return Promise.cast(action())
-                .then(loop)
-                .catch(resolver.reject);
+    Promise.resolve(DoSomethingFirst()) // some starting promise or method 
+        .then(firstFunction)
+        .then(secondFunction)
+
+        .error(function (e) {
+            console.log("Error handler " + e)
+        })
+        .catch(function (e) {
+            console.log("Catch handler " + e)
+        });
+
+
+// F U N C T I O N S  : ########################################################
+
+
+    function DoSomethingFirst() {
+        console.log('doing something first..');
+    }
+
+    function firstFunction() {
+
+        return new Promise(function (resolve) {
+            console.log('Hi! i am first function ');
+            console.log('waiting till first function is finish....');
+            console.log('second function will start only when first function finish (timeout of 7 seconds)!');
+            setTimeout(function () {
+                resolve();
+            }, 7000);
+        });
+
+    }
+
+
+    function secondFunction() {
+
+        // promise while pattern function:
+        var promiseWhile = function (condition, action) {
+            var resolver = Promise.defer();
+
+            var loop = function () {
+                if (!condition()) return resolver.resolve();
+                return Promise.cast(action())
+                    .then(loop)
+                    .catch(resolver.reject);
+            };
+
+            process.nextTick(loop);
+
+            return resolver.promise;
         };
 
-        process.nextTick(loop);
 
-        return resolver.promise;
-    };
+        var current = 0;
+        var stop = 5;
 
-    var current = 0;
-    var stop = 50;
+        promiseWhile(function () {
+            // Condition for stopping
+            return current < stop;
+        }, function () {
+            // Action to run, should return a promise
+            return new Promise(function (resolve, reject) {
 
-    promiseWhile(function () {
-        // Condition for stopping
-        return current < stop;
-    }, function () {
-        // Action to run, should return a promise
-        return new Promise(function (resolve, reject) {
+                var count = 0;
 
-            var count = 0;
+                async.whilst(
+                    function () {
+                        return count < 1;
+                    },
+                    function (callback) {
 
-            async.whilst(
-                function ()
-                { return count < 1; },
-                function (callback) {
-                  
-                    fillCollection(current);
-                    count++;
-                    current++;
+                        fillCollection(current);
+                        count++;
+                        current++;
 
-                    setTimeout(callback, 500); // better minimum 500 for timeOut so things go smooth
-                },
-                function (err) {
-                    resolve();
-                }
-            );
+                        setTimeout(callback, 500); // better minimum 500 for timeOut so things go smooth
+                    },
+                    function (err) {
+                        resolve();
+                    }
+                );
+
+            })
+
+        }).then(function () {
+
+            console.log('');
+            console.log('here will execute another thing after the previous loop');
+
+            var current = 0;
+            var stop = 10;
+
+            promiseWhile(function () {
+                // Condition for stopping
+                return current < stop;
+            }, function () {
+                // Action to run, should return a promise
+                return new Promise(function (resolve, reject) {
+
+                    var count = 0;
+
+                    async.whilst(
+                        function () {
+                            return count < 1;
+                        },
+                        function (callback) {
+
+                            fillCollection(current);
+                            count++;
+                            current++;
+
+                            setTimeout(callback, 500); // better minimum 500 for timeOut so things go smooth
+                        },
+                        function (err) {
+                            resolve();
+                        }
+                    );
+
+                })
+
+            }).then(function () {
+
+                console.log('');
+                console.log('you can chain over and over adding another \'then\'. they will be executed after the previous then ');
+                console.log('this will be the FINAL line ');
+            })
             
 
         })
 
-    }).then(function () {
+    }
 
-        console.log('');
-        console.log('here will execute another thing after the previous loop');
-
-    }).then(function () {
-
-        console.log('');
-        console.log('you can chain over and over adding another \'then\'. they will be executed after the previous then ');
-        console.log('this will be the FINAL line ');
-
-    })
 
 }
 
